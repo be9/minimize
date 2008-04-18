5c8a325708f4b2fd32533a90c68a6a85776bde94
oleg dashevskii <be9@be9.ru>
2008-04-18 13:35:20 +0700

    Add Woods function (from hooke.c), also works

--- nmtest.cpp
+++ nmtest.cpp
@@ -8,7 +8,10 @@ using namespace std;
 using namespace minimize;
 using namespace minimize::nelder_mead;
 
-double rosenbrocks(const params_vector &params)
+#define WOODS
+
+#ifndef WOODS
+double func(const params_vector &params)
 {
     assert(params.size() == 2);
 
@@ -16,6 +19,46 @@ double rosenbrocks(const params_vector &params)
 
     return (1.0-x)*(1.0-x) + 100 * (y-x*x)* (y-x*x);
 }
+
+params_vector initial()
+{
+    params_vector x0(2);
+
+    x0[0] = 100.0;
+    x0[1] = 100.0;
+
+    return x0;
+}
+#else
+/* Woods -- a la More, Garbow & Hillstrom (TOMS algorithm 566) */
+
+double func(const params_vector &x)
+{
+	double s1, s2, s3, t1, t2, t3, t4, t5;
+	s1 = x[1] - x[0]*x[0];
+	s2 = 1 - x[0];
+	s3 = x[1] - 1;
+	t1 = x[3] - x[2]*x[2];
+	t2 = 1 - x[2];
+	t3 = x[3] - 1;
+	t4 = s3 + t3;
+	t5 = s3 - t3;
+	return 100*(s1*s1) + s2*s2 + 90*(t1*t1) + t2*t2 + 10*(t4*t4) + t5*t5/10.;
+}
+
+params_vector initial()
+{
+    params_vector x0(4);
+	
+    x0[0] = -3;
+	x0[1] = -1;
+	x0[2] = -3;
+	x0[3] = -1;
+
+    return x0;
+}
+
+#endif
 
 bool reporter(report_stage stage, report_details details, int iter, int func_evals, 
         const values_vec &values, const simplex_mat &simplex)
@@ -43,14 +86,9 @@ bool reporter(report_stage stage, report_details details, int iter, int func_eva
 
 int main()
 {
-    params_vector x0(2);
-
-    x0[0] = 100.0;
-    x0[1] = 100.0;
-    
     output out;
 
-    run(x0, rosenbrocks, out, reporter);
+    run(initial(), func, out, reporter);
 
     static const char *return_codes[] = {
         "SUCCESS", "ABORT", "MAX_FUNC_EVALS", "MAX_ITER"
